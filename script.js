f743d8c5416925cf6ef22898f9cd97c07ac975db
RITU <ritu169007@gmail.com>
2026-06-27 20:37:28 +0530

    Upload Scientific Calculator project with README

---
+++ script.js
@@ -0,0 +1,80 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button'); 
+let string = "";   
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => { 
+
+        if(e.target.innerHTML == '='){  
+         string = eval(string);  
+         input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){ 
+            string ="";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){  
+        string = string.substring(0, string.length-1); 
+        input.value = string; 
+        }
+        else if(e.target.innerHTML.trim() == 'abs'){
+        let num = Number(input.value);
+        let result = Math.abs(num);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == 'sin'){
+        let num = Number(input.value);
+        let rad = num * (Math.PI / 180);
+        let result = Math.sin(rad);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == 'cos'){
+        let num = Number(input.value);
+        let rad = num * (Math.PI / 180);
+        let result = Math.cos(rad);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == 'tan'){
+        let num = Number(input.value);
+        let rad = num * (Math.PI / 180);
+        let result = Math.tan(rad);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == 'log₁₀(x)'){
+        let num = Number(input.value);
+        let result = Math.log10(num);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == 'ln(x)'){
+        let num = Number(input.value);
+        let result = Math.log(num);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == '√x'){
+        let num = Number(input.value);
+        let result = Math.sqrt(num);
+        string = result.toString();
+        input.value = string;
+        }
+        else if(e.target.innerHTML.trim() == 'π'){
+        string = Math.PI.toString();
+        input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+});
+let toggleButton = document.getElementById('toggleButton');
+let extraOptions = document.getElementById('extraOptions');
+ toggleButton.addEventListener("click",()=>{
+        extraOptions.classList.toggle("show");
+        console.log(extraOptions.className);
+       toggleButton.innerHTML = extraOptions.classList.contains("show")? "▲" : "▼";
+ });
